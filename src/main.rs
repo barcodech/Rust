22457e80d4f6cc160e673051a416006d79105640
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-21 18:12:28 +0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+    let x = 5;
+    x = 10;
+ 
+    let mut x = 5;
+`    x = 10;
+ 
+    let x = 5;
+    let x = x+3;
+
+    let mut x = 1;
+    println!("value of x: {}",x);
+    x =2;
+    println!("value of x: {}",x);
+
+    let y = true;
+    println!("value of y: {}",y);
+    let y = false;
+    println!("value of y: {}",y);
+
+    const STRING: &str = "hello";
+    println!("value of STRING: {}",STRING);
+
+}
