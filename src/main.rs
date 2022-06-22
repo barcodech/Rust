957a72f87505f4918a3c46a5166a1d5ccec20d1b
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-22 11:30:45 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,16 @@ fn main() {
 
     println!("{}",***references3 == my_number);
 }
+
+---------------------------pointer_ref
+fn main() {
+    let arr1 = [1,2,3];
+    let arr2 = arr1;
+
+    println!("Value: {:?}",(arr1,arr2));
+
+    let vec1 = vec![1,2,3];
+    let vec2 = &vec1;
+
+    println!("Value: {:?}",(&vec1,vec2));
+}
