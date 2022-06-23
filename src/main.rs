7ef603c3e1c6c63746699eec544abbbda1bcf0c9
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-23 16:24:09 +0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+const MY_NUMBER: u32 = 12; 
+static MY_DAYS: [&str; 4] = ["Monday", "Sunday", "Tuesday", "Wednesday"];
+
+
+
+fn main(){
+  println!("{}",MY_NUMBER);
+  println!("{:?}",MY_DAYS);
+  
+}
