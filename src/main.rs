c3f2d82d61c0d943be2125ca36ab64d2f105d7f6
barcodech <103811940+barcodech@users.noreply.github.com>
2022-07-01 13:48:34 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,78 @@ fn main(){
   println!("Did you study Java? {}", marks.contains_key("Java"))
 
 }
+
+use std::collections::HashMap;
+ 
+fn main() {
+    let mut drink_hashmap = HashMap::new();
+ 
+    drink_hashmap.insert(1, "cola");
+    drink_hashmap.insert(1, "coffee");
+    drink_hashmap.insert(1, "pepsi");
+    drink_hashmap.insert(1, "est");
+ 
+    println!("{:?}", drink_hashmap.get(&1));
+}
+ 
+use std::collections::HashMap;
+ 
+fn main() {
+    let drink_vec = vec!["est", "pepsi", "coffee", "coffee"];
+ 
+    let mut drink_hashmap = HashMap::new();
+ 
+    for drink in drink_vec {
+      drink_hashmap.entry(drink).or_insert(true);
+    }
+    for (drink, true_or_false) in drink_hashmap {
+        println!("Do we have {}? {}", drink, true_or_false);
+    }
+}
+ 
+ 
+use std::collections::HashMap;
+ 
+fn main() {
+    let drink_vec = vec!["est", "pepsi", "coffee", "coffee"];
+ 
+ 
+    let mut drink_hashmap = HashMap::new();
+ 
+    for drink in drink_vec {
+        let return_value = drink_hashmap.entry(drink).or_insert(0);
+        *return_value +=1;
+    }
+ 
+    for (drink, number) in drink_hashmap {
+        println!("{}, {}", drink, number);
+    }
+}
+ 
+use std::collections::HashMap;
+ 
+fn main() {
+    let drink_vec = vec![
+        ("cola", 20),
+        ("coffee", 25),
+        ("est", 10),
+        ("coffee", 30),
+        ("est", 15),
+        ("cola", 25),
+    ];
+ 
+    let mut drink_hashmap = HashMap::new();
+ 
+    for drink in drink_vec {
+      drink_hashmap.entry(drink.0).or_insert(Vec::new()).push(drink.1);
+    }
+ 
+    for (drink, prices) in drink_hashmap {
+        println!("{:?}: {:?}", drink, prices);
+    }
+}
+ 
+ 
+ 
+ 
+ 
