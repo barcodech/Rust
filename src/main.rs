21a47247f77db0656ab37c464209ff2f6778746c
barcodech <103811940+barcodech@users.noreply.github.com>
2022-07-03 16:18:33 +0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main(){
+
+    let fruit = "mango";
+
+    //assert!(fruit == "durian","{} should be durian",fruit);
+
+    //assert_eq!(fruit,"durian","{} should be equal durian",fruit);
+
+    assert_ne!(fruit, "mango","{} must not equal mango",fruit);
+} 
